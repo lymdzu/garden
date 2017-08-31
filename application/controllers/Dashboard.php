df71fd0601f84168d8b1092ed3e2145faa7d877e
liuyongming <liuyongming@shopex.cn>
2017-08-31 16:11:07 +0800

    更改主页输出

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -11,6 +11,10 @@ class Dashboard extends PublicController
 //        $this->vars['footer'] = true;
 //        $this->vars['fix_footer'] = true;
 //        $this->page('dashboard/front.html');
-        $this->display("jianli/jianli.html");
+        ob_end_clean();
+        $pdf = file_get_contents(dirname(BASEPATH) . "/public/resume/resume_and_works_of_raoxiaohua.pdf");
+        header('Content-Type: application/pdf');
+        echo $pdf;
+        //$this->display("jianli/jianli.html");
     }
 }
