de2f5ffc557bdaa3dde23c34af021d01da9ffa1b
liuyongming <liuyongming@shopex.cn>
2017-12-08 18:30:39 +0800

    恢复小区

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -8,13 +8,13 @@ class Dashboard extends PublicController
 {
     public function index()
     {
-//        $this->vars['footer'] = true;
-//        $this->vars['fix_footer'] = true;
-//        $this->page('dashboard/front.html');
-        ob_end_clean();
-        $pdf = file_get_contents(dirname(BASEPATH) . "/public/resume/resume_and_works_of_raoxiaohua0914.pdf");
-        header('Content-Type: application/pdf');
-        echo $pdf;
+        $this->vars['footer'] = true;
+        $this->vars['fix_footer'] = true;
+        $this->page('dashboard/front.html');
+//        ob_end_clean();
+//        $pdf = file_get_contents(dirname(BASEPATH) . "/public/resume/resume_and_works_of_raoxiaohua0914.pdf");
+//        header('Content-Type: application/pdf');
+//        echo $pdf;
         //$this->display("jianli/jianli.html");
     }
 }
